82c6c770efd03366a6468df032734a853268a93c
Vova(Vadinhao)Ruzhevich <vova.ruzhevich@gmail.com>
2021-04-02 15:40:53 +0300

    lb2 code

---
+++ lb3/Source.cpp
@@ -0,0 +1,134 @@
+#include "Header.h";
+
+Triangle::Triangle()
+{
+    A = 0;
+    B = 0;
+    C = 0;
+    perimeter = 0;
+    square = 0;
+    angle = new double[3]{ 0,0,0 };
+}
+
+Triangle::Triangle(double sides[])
+{
+    A = sides[0];
+    B = sides[1];
+    C = sides[2];
+    perimeter = Perimeter();
+    square = Square();
+    angle = new double[3];
+    angle = Angle(angle);
+}
+
+Triangle::Triangle(Triangle* triangle)
+{
+    A = triangle->A;
+    B = triangle->B;
+    C = triangle->C;
+    perimeter = triangle->perimeter;
+    square = triangle->square;
+    angle = new double[3];
+    angle = triangle->angle;
+}
+
+Triangle::~Triangle()
+{
+    //
+}
+
+void Triangle::Set(double sides[])
+{
+    A = sides[0];
+    B = sides[1];
+    C = sides[2];
+    perimeter = Perimeter();
+    square = Square();
+    angle = new double[3];
+    angle = Angle(angle);
+}
+
+double Triangle::Get_A() const
+{
+    return A;
+}
+
+double Triangle::Get_B() const
+{
+    return B;
+}
+
+double Triangle::Get_C() const
+{
+    return C;
+}
+
+double Triangle::Get_Perimeter() const
+{
+    return perimeter;
+}
+
+double Triangle::Get_Square() const
+{
+    return square;
+}
+
+double* Triangle::Get_Angle() const
+{
+    return angle;
+}
+
+bool Triangle::Istriangle() const
+{
+    return (A + B > C) && (B + C > A) && (C + A > B);
+}
+
+Triangle Triangle::operator*(double k)
+{
+    Triangle temp(this);
+    temp.A = temp.A * k;
+    temp.B = temp.B * k;
+    temp.C = temp.C * k;
+    return temp;
+}
+
+Triangle operator*(double k, Triangle tr)
+{
+    double sides[3]{ tr.A * k, tr.B * k, tr.C * k };
+    Triangle temp = new Triangle(sides);
+    return temp;
+}
+
+string Triangle::toString() const
+{
+    string s1;
+    std::ostringstream ostr;
+    ostr << "A: " << A << ", B: " << B << ", C: " << C << "\nPerimeter: " << perimeter << "\nSquare: " << square << "\nAngles(a,b,c): " << angle[0] << ", " << angle[1] << ", " << angle[2];
+    s1 = ostr.str();
+    return s1;
+}
+
+ostream& operator<<(ostream& os, const Triangle& tr)
+{
+    os << "A: " << tr.A << ", B: " << tr.B << ", C: " << tr.C << "\nPerimeter: " << tr.perimeter << "\nSquare: " << tr.square << "\nAngles(a,b,c): " << tr.angle[0] << ", " << tr.angle[1] << ", " << tr.angle[2];
+    return os;
+}
+
+double Triangle::Perimeter() const
+{
+    return	this->A + this->B + this->C;
+}
+
+double Triangle::Square() const
+{
+    double p = Triangle::Perimeter() / 2;
+    return sqrt(p * (p - this->A) * (p - this->B) * (p - this->C));
+}
+
+double* Triangle::Angle(double* angl)
+{
+    angl[0] = 180 * acos((this->B * (this->B) + this->C * (this->C) - this->A * (this->A)) / (2 * (this->B) * (this->C))) / PI;
+    angl[1] = 180 * acos((this->C * (this->C) + this->A * (this->A) - this->B * (this->B)) / (2 * (this->C) * (this->A))) / PI;
+    angl[2] = 180 * acos((this->A * (this->A) + this->B * (this->B) - this->C * (this->C)) / (2 * (this->A) * (this->B))) / PI;
+    return angl;
+}
--- lb3/lb3.cpp
+++ lb3/lb3.cpp
@@ -1,20 +1,119 @@
-﻿// lb3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿// lb3.cpp
+#include "Header.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "rus");
+    cout << "Вариант 1(18-й)" << endl;
+    double sides[3]{ 0,0,0 };
+    bool b = true;
+    int ind = 0, ch = 0;
+    vector <Triangle> vec_triangle;
+    while (1)
+    {
+        cout << "Вводите длины сторон треугольников(0 для продолжения)" << endl;
+        while (1)
+        {
+            cout << "A: ";
+            cin >> sides[0];
+            if (sides[0] == 0) { break; }
+
+            cout << "B: ";
+            cin >> sides[1];
+            if (sides[1] == 0) { break; }
+
+            cout << "C: ";
+            cin >> sides[2];
+            if (sides[2] == 0) { break; }
+            Triangle temp(sides);
+            if (temp.Istriangle())
+            {
+                vec_triangle.push_back(temp);
+            }
+            else
+                cout << "Вы ввели не треугольник, попробуйте по новой." << endl;
+            cout << "___________________" << endl;
+        }
+        cout << "Ваши треугольники" << endl;
+        for (int i = 0; i < vec_triangle.size(); i++)
+            cout << "*******************\n" << "*" << i + 1 << "*\n" << vec_triangle[i] << "\n*******************" << endl;
+        while (b)
+        {
+            ind = 0;
+            do {
+                cout << "Выберите треугольник или введите 0 для выхода в начало: " << endl;
+                cin >> ind;
+                ind--;
+                if (ind == -1)
+                {
+                    b = false;
+                    break;
+                }
+            } while (ind < 0 && ind >= vec_triangle.size());
+            if (ind == -1)
+                continue;
+            cout << "Выберите действие для работы с треугольниками:\n0. Выход в начало программы\n1. Умножить стороны треугольника на коефициент.\n2. Вывести ваши треугольники.\n3. Добавить элемент\n4. Удалить элемент." << endl;
+            cin >> ch;
+            if (ch == 0)
+            {
+                b = false;
+                continue;
+                break;
+            }
+            if (ch == 1)
+            {
+                cout << ind << endl;
+                cout << "Введите на сколько стоит умножить стороны треугольника: " << endl;
+                cin >> ch;
+                vec_triangle[ind] = vec_triangle[ind] * ch; //метод
+                //vec_triangle[ind] = ch * vec_triangle[ind];//друж функция
+                cout << "Новые стороны треугольника: " << vec_triangle[ind].Get_A() << " " << vec_triangle[ind].Get_B() << " " << vec_triangle[ind].Get_C() << endl;
+            }
+            else if (ch == 2)
+            {
+                cout << "Ваши треугольники и их парамеры: " << endl;
+                for (int i = 0; i < vec_triangle.size(); i++)
+                    cout << "*******************\n" << "*" << i + 1 << "*\n" << vec_triangle[i].toString() << "\n*******************" << endl;
+            }
+            else if (ch == 3)
+            {
+                cout << "A: ";
+                cin >> sides[0];
+                if (sides[0] == 0) { break; }
+
+                cout << "B: ";
+                cin >> sides[1];
+                if (sides[1] == 0) { break; }
+
+                cout << "C: ";
+                cin >> sides[2];
+                if (sides[2] == 0) { break; }
+                Triangle temp(sides);
+                if (temp.Istriangle())
+                {
+                    auto iter = vec_triangle.cbegin();
+                    vec_triangle.emplace(iter + ind, temp);
+                    cout << "Ваши треугольники и их парамеры: " << endl;
+                    for (int i = 0; i < vec_triangle.size(); i++)
+                        cout << "*******************\n" << "*" << i + 1 << "*\n" << vec_triangle[i].toString() << "\n*******************" << endl;
+                }
+                else
+                    cout << "Вы ввели не треугольник, попробуйте по новой." << endl;
+            }
+            else if (ch == 4)
+            {
+                auto iter = vec_triangle.cbegin();
+                vec_triangle.erase(iter + ind);
+                cout << "Ваши треугольники и их парамеры: " << endl;
+                for (int i = 0; i < vec_triangle.size(); i++)
+                    cout << "*******************\n" << "*" << i + 1 << "*\n" << vec_triangle[i].toString() << "\n*******************" << endl;
+            }
+        }
+        cin.clear();
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        string sss;
+        cout << "Нажмите 0 для выхода или другую клавишу для продолжения..." << endl;
+        getline(cin, sss);
+        if (sss == "0") { break; }
+    }
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
